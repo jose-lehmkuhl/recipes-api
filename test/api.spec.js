dadd0a48c595e6026ba7f76f770b01b6cd22eb8d
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2020-07-31 20:48:02 -0300

    test: Ingredients Error From /recipes/

--- test/api.spec.js
+++ test/api.spec.js
@@ -3,9 +3,23 @@
 const chai = require('chai');
 
 const { expect } = chai;
+const chaiHttp = require('chai-http');
+const app = require('../src/app');
+
+chai.use(chaiHttp);
+
+const requester = chai.request(app).keepOpen();
 
 describe('API tests', () => {
-  it('should run tests boilerplate', async () => {
+  after(async () => {
+    requester.close();
+  });
+
+  it('should get an error response from /recipes/', async () => {
     expect(true).to.be.eq(true);
+    const response = await requester.get('/recipes/');
+
+    expect(response.statusCode).to.be.equal(400);
+    expect(response.body.error).to.be.equal('Please provide valid ingredients as query parameters');
   });
 });
