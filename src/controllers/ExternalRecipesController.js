d27915e1454de5cd261188e91573c9e0d27c5cac
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2020-08-01 13:50:17 -0300

    Add get recipes logic, axios recipe request and passed test

--- src/controllers/ExternalRecipesController.js
+++ src/controllers/ExternalRecipesController.js
@@ -1,3 +1,19 @@
+const { recipeRequest } = require('../services/axios');
+
 const ExternalRecipesController = {};
+
+ExternalRecipesController.baseUrl = 'http://www.recipepuppy.com/api/';
+
+ExternalRecipesController.successResponse = (recipes) => ({
+  status: 200,
+  recipes,
+});
+
+ExternalRecipesController.getRecipes = async (ingredients) => {
+  const response = await recipeRequest(ExternalRecipesController.baseUrl, ingredients);
+  if (response.status === 200) return ExternalRecipesController.successResponse(response.data.results);
+
+  return response;
+};
 
 module.exports = ExternalRecipesController;
