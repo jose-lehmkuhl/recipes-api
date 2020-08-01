72f24cd9a16ba6f4c5183974e2da8bf2f8455b1a
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2020-07-31 21:03:29 -0300

    test: Add ingredients limitation

--- test/api.spec.js
+++ test/api.spec.js
@@ -15,11 +15,17 @@ describe('API tests', () => {
     requester.close();
   });
 
-  it('should get an error response from /recipes/', async () => {
-    expect(true).to.be.eq(true);
+  it('should get an error response from /recipes/ when missing "i" parameter', async () => {
     const response = await requester.get('/recipes/');
 
     expect(response.statusCode).to.be.equal(400);
-    expect(response.body.error).to.be.equal('Please provide valid ingredients as query parameters');
+    expect(response.body.error).to.be.equal('Missing ingredients parameters');
+  });
+
+  it('should get an error response from /recipes/ when passing more than 3 ingredients', async () => {
+    const response = await requester.get('/recipes/?i=ingredient1,ingredient2,ingredient3,ingredient4');
+
+    expect(response.statusCode).to.be.equal(400);
+    expect(response.body.error).to.be.equal('Maximum of 3 ingredients allowed');
   });
 });
