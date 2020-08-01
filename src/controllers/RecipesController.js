0d5614f744204b5c169e33216c20ce3a9a91fe9f
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2020-08-01 14:31:09 -0300

    shore: Reestructured recipes controller

--- src/controllers/RecipesController.js
+++ src/controllers/RecipesController.js
@@ -15,23 +15,21 @@ RecipesController.externalError = (externalResponse, res) => res.status(external
   response: externalResponse,
 });
 
+RecipesController.externalRecipeHandler = async (ingredients, res) => {
+  const recipesResponse = await getRecipes(ingredients);
+  if (recipesResponse.status !== 200) return RecipesController.externalError(recipesResponse, res);
+  const { recipes } = recipesResponse;
+  if (recipes.length === 0) return res.status(200).json(RecipesController.responseObject(ingredients.split(','), recipes));
+};
+
 RecipesController.getRecipes = async (req, res) => {
   const { i: ingredients } = req.query;
 
   if (ingredients === undefined) return RecipesController.recipesError('Missing ingredients parameters', res);
-
   const ingredientsArray = ingredients.split(',');
-
   if (ingredientsArray.length > 3) return RecipesController.recipesError('Maximum of 3 ingredients allowed', res);
 
-  const recipesResponse = await getRecipes(ingredients);
-
-  if (recipesResponse.status !== 200) return RecipesController.externalError(recipesResponse, res);
-
-  const { recipes } = recipesResponse;
-  if (recipes.length === 0) return res.status(200).json(RecipesController.responseObject(ingredientsArray, recipes));
-
-  return res.send(200);
+  return RecipesController.externalRecipeHandler(ingredients, res);
 };
 
 module.exports = RecipesController;
