ed89c0687728ea5aeb65429aeaf3632885f99118
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2020-08-01 14:16:26 -0300

    Handled external recipes responses and passes test

--- src/controllers/RecipesController.js
+++ src/controllers/RecipesController.js
@@ -1,15 +1,36 @@
+const { getGif } = require('./GifController');
+const { getRecipes } = require('./ExternalRecipesController');
+
 const RecipesController = {};
 
-RecipesController.invalidIngredientsError = (message, res) => res.status(400).json({ error: message });
+RecipesController.recipesError = (message, res) => res.status(400).json({ error: message });
+
+RecipesController.responseObject = (keywords, recipes) => ({
+  keywords,
+  recipes,
+});
+
+RecipesController.externalError = (externalResponse, res) => res.status(externalResponse.status).json({
+  error: 'Error received from external API',
+  response: externalResponse,
+});
 
 RecipesController.getRecipes = async (req, res) => {
   const { i: ingredients } = req.query;
 
-  if (ingredients === undefined) return RecipesController.invalidIngredientsError('Missing ingredients parameters', res);
+  if (ingredients === undefined) return RecipesController.recipesError('Missing ingredients parameters', res);
 
   const ingredientsArray = ingredients.split(',');
 
-  if (ingredientsArray.length > 3) return RecipesController.invalidIngredientsError('Maximum of 3 ingredients allowed', res);
+  if (ingredientsArray.length > 3) return RecipesController.recipesError('Maximum of 3 ingredients allowed', res);
+
+  const recipesResponse = await getRecipes(ingredients);
+
+  if (recipesResponse.status !== 200) return RecipesController.externalError(recipesResponse, res);
+
+  const { recipes } = recipesResponse;
+  if (recipes.length === 0) return res.status(200).json(RecipesController.responseObject(ingredientsArray, recipes));
+
   return res.send(200);
 };
 
