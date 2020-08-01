84bd112bcaaf1509634aad71d01b0f881dc4006d
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2020-08-01 15:48:48 -0300

    added Error handling to external requests

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,9 +2,13 @@ const axios = require('axios');
 require('dotenv/config');
 
 const gifApiKey = process.env.GIF_API_KEY;
+const defaultErrorHandling = (api) => ({
+  status: 500,
+  error: `External ${api} api Error`,
+});
 
-const gifRequest = (gifApiUrl, recipeName) => axios.get(`${gifApiUrl}?q=${recipeName}&api_key=${gifApiKey}&limit=1`);
-const recipeRequest = (recipesApiUrl, ingredients) => axios.get(`${recipesApiUrl}?i=${ingredients}`);
+const gifRequest = (gifApiUrl, recipeName) => axios.get(`${gifApiUrl}?q=${recipeName}&api_key=${gifApiKey}&limit=1`).catch(() => defaultErrorHandling('gif'));
+const recipeRequest = (recipesApiUrl, ingredients) => axios.get(`${recipesApiUrl}?i=${ingredients}`).catch(() => defaultErrorHandling('recipe'));
 
 module.exports = {
   gifRequest,
